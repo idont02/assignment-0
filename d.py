361bcdea764ddb2af6b8f4b70a1b068e760fc59e
idont02 <216201600@xinzhong.sch.id>
2025-02-09 17:03:05 +0700

    Create d.py

---
+++ d.py
@@ -0,0 +1,9 @@
+print('type 1 for square')
+print('type 2 for rectangle')
+opt = int(input('type here: '))
+if opt == 1:
+  s = int(input('type side length: '))
+  print('The area of the square is',s**2)
+elif opt == 2:
+  l = int(input('type length: '))
+  b = int(input('type breadth: '))
