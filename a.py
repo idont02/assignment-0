9aeed4a8bf2a2a78baee43bc3553d29ffa85ccbd
idont02 <216201600@xinzhong.sch.id>
2025-02-09 16:13:01 +0700

    Create a.py

---
+++ a.py
@@ -0,0 +1,10 @@
+n1 = int(input("type first number: "))
+n2 = int(input("type second number: "))
+if n1 > n2:
+  print("First number is greater than second number")
+elif n1 < n2:
+  print("Second number is greater than first number")
+elif n1 == n2:
+  print("numbers are equal")
+else:
+  print("error.")
