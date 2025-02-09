1250eaaa43b74063bd077f4b5b107fcc90ce7f77
idont02 <216201600@xinzhong.sch.id>
2025-02-09 18:41:23 +0700

    Update d.py

--- d.py
+++ d.py
@@ -1,9 +1,11 @@
+print('uses metric system')
 print('type 1 for square')
 print('type 2 for rectangle')
 opt = int(input('type here: '))
 if opt == 1:
   s = int(input('type side length: '))
-  print('The area of the square is',s**2)
+  print('The area of the square is',s**2,'cm^2')
 elif opt == 2:
   l = int(input('type length: '))
   b = int(input('type breadth: '))
+  print('The area of the rectangle is',l*b,'cm^2')
