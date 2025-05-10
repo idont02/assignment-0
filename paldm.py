86027613863422de23cb192efd08f1b75a015865
idont02 <216201600@xinzhong.sch.id>
2025-05-10 16:22:26 +0700

    Create paldm.py

---
+++ paldm.py
@@ -0,0 +1,14 @@
+print('Type a number')
+a = input()
+try:
+  int(a)
+except:
+  print('Error, try again.')
+c = a
+l=len(a)
+for i in range(l):
+  rm=a%10
+  r=r*10+rm
+  a=a//10
+if c == r:
+  print('It is a palindrome.')
