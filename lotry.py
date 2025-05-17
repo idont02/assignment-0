b48cd141ce7fc8397a58bc1596b3fecfd76bb21c
idont02 <216201600@xinzhong.sch.id>
2025-05-17 16:12:15 +0700

    Update lotry.py
    ok so i know its a downgrade...

--- lotry.py
+++ lotry.py
@@ -1,42 +1,20 @@
 from random import randint
-def checkint():
-   while True:
-    n = input()
+print('welcome to lotery bot')
+print('pick a number, then the bot will pick a number, if it is the same, you win.')
+print('pick from 1 - 100')
+r = randint(1,100)
+print(r)
+print(type(r))
+n = input()
+while True:
     try:
-      int(n)
-      break
+        n = int(n)
+        break
     except:
-      print('Error, try again.\nDISCLAIMER: This failure is caused by an ERROR.')
-    finally:
-      input('Press ENTER to proceed.')
-def main(r):
-   while True:
-      print('Pick a number between 1 -',r2)
-      checkint()
-      input('Press ENTER to proceed.')
-    lot = randint(1,r2)
-    if n == lot:
-      print('That is a lottery! You win!')
-    else:
-      print('That is not the number. You lose.')
-    print('The number was',lot,'\nType 1 to retry\nType 0 to exit')
-    while True:
-      re = input()
-      checkint()
-      if re == 1 or re == 0:
-        pass
-      else:
-        print('Please type 0 or 1.')
-    if re == 0:
-      break
-input('welcome to the lottery bot\nThe rules are simple: You and the bot will guess a number. If it matches, you win!\nPress ENTER\n')
-print('Select difficulty:\n\nPossible\nVery Easy\nEasy\nNormal\nHard\nVery Hard\nActually a lottery\nImpossible\n')
-d = input()
-d=d.lower()
-if d == 'possible':
-  main(10)
-elif d == 'very easy':
-  main(30)
-  
-  
-      
+        input('Not a whole number. Press ENTER.')
+print(type(n))
+
+if n == r:
+    print('That is the lottery! YOU WIN!!')
+else:
+    print('That is not the lottery. The number was',r,'.')
