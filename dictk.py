89ba9ef7ef608fbf1361e1e99fa3466818150190
idont02 <216201600@xinzhong.sch.id>
2025-06-04 19:49:12 +0700

    Update dictk.py

--- dictk.py
+++ dictk.py
@@ -1,5 +1,9 @@
-dict = {'00' = 0, '01':1,'02':2}
-print(dict.keys())
-print(dict.get('01'))
+dicts = {'00':0, '01':1,'02':2,'03':3,'09':9,'0a':11,'0f':15,'10':16,'fe':254,'ff':255}
+print(dicts.keys())
+print(dicts.get('01'))
 
-print(dict)
+print(dicts)
+dicts['0a']= 10
+print(dicts)
+
+print(dicts.values())
