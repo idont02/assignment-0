8c4992ec760eba423de11723618dca6087fe5d72
idont02 <216201600@xinzhong.sch.id>
2025-12-13 21:16:23 +0700

    raw texture
    raw of project

---
+++ rqzgm.py
@@ -0,0 +1,10 @@
+print("What is the capital of USA") 
+print("1. New York")
+print("2. Washington DC")
+print("3. San Francisco")
+print("Answer in numeric 1, 2 or 3")
+ans=input()
+if ans == 2:
+    print('\nYay correct')
+else:
+    print('\nWrong, answer is 2')
